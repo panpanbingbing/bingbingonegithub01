6b0afd8d672b479340c1d980ce4ceffd2210a4be
root <root@bingbing.con>
2022-11-13 18:38:03 +0800

    new Test

--- OnesGitHub/src/Main.java
+++ OnesGitHub/src/Main.java
@@ -1,5 +1,11 @@
+import com.bing.domain.Student;
+
 public class Main {
     public static void main(String[] args) {
+
+        Student student = new Student();
+
+
         System.out.println("Hello world!");
     }
 }
